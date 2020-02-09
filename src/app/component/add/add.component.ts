4554ec28c04e9124e464df77316f62d0755fa796
Đinh Đức Giang <changdt.eps@gmail.com>
2020-02-09 22:11:54 +0700

    validate 2 input

--- src/app/component/add/add.component.ts
+++ src/app/component/add/add.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { VocabularyService } from 'src/app/service/vocabulary.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Vocabulary } from 'src/models/Vocabulary';
 
 @Component({
@@ -8,6 +9,7 @@ import { Vocabulary } from 'src/models/Vocabulary';
   styleUrls: ['./add.component.scss']
 })
 export class AddComponent implements OnInit {
+  formcheck;
   newID = this.vocabularyService.vocabularies[(this.vocabularyService.vocabularies.length) - 1].id + 1;
   newEN = '';
   newVN = '';
@@ -15,17 +17,36 @@ export class AddComponent implements OnInit {
   constructor(private vocabularyService: VocabularyService) { }
 
   ngOnInit() {
+    this.formcheck = new FormGroup({
+      'en': new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.pattern('[a-zA-Z ]*')
+      ]),
+      'vn': new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.pattern('[a-zA-Z ]*')
+      ])
+    });
   }
 
+  EN() {return this.formcheck.get('en'); }
+  VN() {return this.formcheck.get('vn'); }
+
   addNew() {
+    // console.log(this.formcheck.get('en').errors);
     const Obj: Vocabulary = {
       id: this.newID,
       en: this.newEN,
       vn: this.newVN,
       memorized: false
     };
-    this.vocabularyService.add(Obj);
-    this.newEN = '';
-    this.newVN = '';
+
+    if ((!this.EN().errors) && (!this.VN().errors)) {
+      this.vocabularyService.add(Obj);
+      this.EN().setValue('');
+      this.VN().setValue('');
+    }
   }
 }
